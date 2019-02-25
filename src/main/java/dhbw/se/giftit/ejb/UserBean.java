c3b37d2cd6d7b241538c38abaab91a79be747175
Sascha Pflittner <s.pflittner@gmail.com>
2019-02-25 14:51:47 +0100

    commit to make source-packages accessable

---
+++ src/main/java/dhbw/se/giftit/ejb/UserBean.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dhbw.se.giftit.ejb;
+
+/**
+ *
+ * @author spfli
+ */
+public class UserBean {
+    
+}
