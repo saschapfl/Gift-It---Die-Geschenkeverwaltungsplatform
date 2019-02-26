907c12056166347a02a9ddb0daac89a3711b18d8
Sven <sven@zelinka.de>
2019-02-26 15:00:45 +0100

    Rename User.jpa to UserEntry; HashGenerator anlegen; UserEntry ausprogrammiert

---
+++ src/main/java/dhbw/se/giftit/web/HashGenerator.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dhbw.se.giftit.web;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author sven
+ */
+public class HashGenerator {
+    
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+    
+        public static String hashPassword(String password) {
+        byte[] hash;
+
+        if (password == null) {
+            password = "";
+        }
+
+        // Hashwert zum Passwort berechnen
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+        } catch (NoSuchAlgorithmException ex) {
+            hash = "!".getBytes(StandardCharsets.UTF_8);
+        }
+
+        // Hashwert in einen Hex-String umwandeln
+        // Vgl. https://stackoverflow.com/a/9855338
+        char[] hashHex = new char[hash.length * 2];
+
+        for (int i = 0; i < hash.length; i++) {
+            int v = hash[i] & 0xFF;
+            hashHex[i * 2] = HEX_ARRAY[v >>> 4];
+            hashHex[i * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+
+        return new String(hashHex);
+    } 
+}
