d74835664a1710f3c8dce71089d0e7e8f7e6540d
Viktoria Swidersky <viktoria.swidersky@gmx.de>
2019-02-27 15:08:03 +0100

    Anlegen von Servlets für Idea und Room

--- src/main/java/dhbw/se/giftit/web/CreateIdeaServlet.java
+++ src/main/java/dhbw/se/giftit/web/CreateIdeaServlet.java
@@ -5,60 +5,45 @@
  */
 package dhbw.se.giftit.web;
 
-import dhbw.se.giftit.ejb.IdeaBean;
-import dhbw.se.giftit.ejb.RoomBean;
-import dhbw.se.giftit.jpa.IdeaEntry;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import javax.ejb.EJB;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author Viktoria
  */
-@WebServlet(name = "RoomView", urlPatterns = {"/RoomView"})
-public class RoomView extends HttpServlet {
-    
-    @EJB
-    IdeaBean ideaBean;
-    
-    @EJB
-    RoomBean roomBean;
-    
-     
-   @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
+@WebServlet(name = "CreateIdeaServlet", urlPatterns = {"/CreateIdeaServlet"})
+public class CreateIdeaServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-        // Dashboard-Rubriken und Kacheln erzeugen und im Request Context ablegen
-        List<IdeaEntry> sections = new ArrayList<>();
-        request.setAttribute("sections", sections);
-        
-        
-
-        // Anfrage an die JSP weiterleiten
-        request.getRequestDispatcher("/WEB-INF/dashboard/dashboard.jsp").forward(request, response);
-    }
-    
-     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        HttpSession session = request.getSession();
-
-        // Neuen Eintrag speichern
-      //  if (fehler.isEmpty()) {
-     //       this.ideaBean.createNewIdea(name);
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet CreateIdeaServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet CreateIdeaServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
         }
-        
-        // Browser auffordern, die Seite neuzuladen
-    //    response.sendRedirect(request.getContextPath());
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
---
+++ src/main/java/dhbw/se/giftit/web/RoomViewServlet.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dhbw.se.giftit.web;
+
+import dhbw.se.giftit.ejb.IdeaBean;
+import dhbw.se.giftit.ejb.RoomBean;
+import dhbw.se.giftit.jpa.IdeaEntry;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Viktoria
+ */
+@WebServlet(name = "RoomView", urlPatterns = {"/RoomView"})
+public class RoomViewServlet extends HttpServlet {
+    
+    @EJB
+    IdeaBean ideaBean;
+    
+    @EJB
+    RoomBean roomBean;
+    
+     
+   @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Dashboard-Rubriken und Kacheln erzeugen und im Request Context ablegen
+        List<IdeaEntry> sections = new ArrayList<>();
+        request.setAttribute("sections", sections);
+        
+        
+
+        // Anfrage an die JSP weiterleiten
+        request.getRequestDispatcher("/WEB-INF/dashboard/dashboard.jsp").forward(request, response);
+    }
+    
+ @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String button = request.getParameter("button");
+         switch (button) {
+            case "createIdea":
+               response.sendRedirect(request.getContextPath() + "/CreateIdeaServlet");
+                break;
+            case "deleteIdea":
+                 response.sendRedirect(request.getContextPath() + "/IdeaViewServlet");
+                break;
+            case "deleteRoom":
+                roomBean.deleteWaste(0);
+                break;
+            default:
+                break;
+        }
+
+        request.getRequestDispatcher("/WEB-INF/some-result.jsp").forward(request, response);
+    }
+}
+
